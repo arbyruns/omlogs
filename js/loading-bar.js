facdc1c79e16108e953a8ebf099980ec6d753c39
Rob Evans <robevans@microsoft.com>
2019-06-11 08:51:28 -0700

    testing loading bar

---
+++ js/loading-bar.js
@@ -0,0 +1,5 @@
+const spinner = document.querySelector('.js-loading-spinner');
+const copy    = spinner.querySelector('.js-loading-spinner-copy');
+if(el.spinner.classList.contains("-loaded")) {
+    copy.innerHTML = "Content has loaded.";
+}
